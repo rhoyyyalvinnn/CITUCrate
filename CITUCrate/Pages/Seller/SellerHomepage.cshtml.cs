373d492a30602f043ae9be85f1c1465d29fc2f5a
rhoyyyalvinnn <dontmisstheblock@gmail.com>
2024-11-23 19:54:49 +0800

    Added sort by function on homepage and fixed css seller page

--- CITUCrate/Pages/Seller/SellerHomepage.cshtml.cs
+++ CITUCrate/Pages/Seller/SellerHomepage.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using CITUCrate.Migrations;
+using Microsoft.Data.SqlClient;
 
 namespace CITUCrate.Pages.Seller
 {
@@ -23,19 +24,57 @@ namespace CITUCrate.Pages.Seller
         public string Username { get; set; }
         public List<Product> Products { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string sortOrder)
         {
             Username = HttpContext.Session.GetString("Username") ?? "Guest";
             var user = await _context.Users.FirstOrDefaultAsync(u => u.Username == Username);
 
             decimal balance = user.Balance;
-
             ViewData["UserBalance"] = balance;
 
-            Products = await _context.Products?.ToListAsync() ?? new List<Product>();
+            // Define sorting parameters
+            ViewData["NameSortParm"] = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewData["CategorySortParm"] = sortOrder == "Category" ? "category_desc" : "Category";
+            ViewData["PriceSortParm"] = sortOrder == "Price" ? "price_desc" : "Price";
+            ViewData["QuantitySortParm"] = sortOrder == "Quantity" ? "quantity_desc" : "Quantity";
+
+            var products = from p in _context.Products
+                           select p;
+
+            // Apply sorting based on the sortOrder parameter
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    products = products.OrderByDescending(p => p.Name);
+                    break;
+                case "Category":
+                    products = products.OrderBy(p => p.Category);
+                    break;
+                case "category_desc":
+                    products = products.OrderByDescending(p => p.Category);
+                    break;
+                case "Price":
+                    products = products.OrderBy(p => p.Price);
+                    break;
+                case "price_desc":
+                    products = products.OrderByDescending(p => p.Price);
+                    break;
+                case "Quantity":
+                    products = products.OrderBy(p => p.Quantity);
+                    break;
+                case "quantity_desc":
+                    products = products.OrderByDescending(p => p.Quantity);
+                    break;
+                default:
+                    products = products.OrderBy(p => p.Name);
+                    break;
+            }
+
+            Products = await products.ToListAsync();
 
             _logger.LogInformation("Number of products fetched: {ProductCount}", Products.Count);
         }
+
 
     }
 }
